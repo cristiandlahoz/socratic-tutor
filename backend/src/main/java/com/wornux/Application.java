77a187e043b14634d17c0c9bed243fbdd76cdaba
Cristian de la Hoz <cyrondelahoz@gmail.com>
2026-03-08 23:51:36 -0400

    wip:

--- backend/src/main/java/com/wornux/Application.java
+++ backend/src/main/java/com/wornux/Application.java
@@ -5,8 +5,11 @@ import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.component.page.BodySize;
 import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.theme.aura.Aura;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @Push
 @BodySize(height = "100vh", width = "100vw")
@@ -19,9 +22,12 @@ public class Application implements AppShellConfigurator {
         SpringApplication.run(Application.class, args);
     }
 
-//    @Bean
-//    ApplicationRunner runner(VectorStore store){
-//        return (_) -> store.similaritySearch("search_query: for loop");
-//    }
+    @Bean
+    ApplicationRunner runner(VectorStore store){
+        return (_) -> store.similaritySearch("""
+            Instruct: Given a student question about C programming,
+            retrieve the most relevant educational passages that answer the question.
+            Query: que es un bucle""");
+    }
 
 }
