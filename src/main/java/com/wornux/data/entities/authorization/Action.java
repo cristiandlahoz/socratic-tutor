71c9cd690b5095a02ecd5c087e010186019959d6
manuujrodcruz <mrmanueljoserodriguezcruz@gmail.com>
2026-06-22 08:35:17 -0400

    feat(authorization): add Role, Resource, Action, Permission entities

---
+++ src/main/java/com/wornux/data/entities/authorization/Action.java
@@ -0,0 +1,41 @@
+package com.wornux.data.entities.authorization;
+
+import java.time.Instant;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "action")
+@Getter
+@Setter
+public class Action {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String code;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private boolean active;
+
+    @Column(name = "created_at", nullable = false)
+    private Instant createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private Instant updatedAt;
+}
