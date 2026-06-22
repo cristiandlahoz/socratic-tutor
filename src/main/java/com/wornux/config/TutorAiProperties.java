9f887dadc389f9e519ce6d8fcd6fb270dc5f3a53
manuujrodcruz <mrmanueljoserodriguezcruz@gmail.com>
2026-06-22 08:34:48 -0400

    chore(config): update TutorAiProperties to explicit accessors

--- src/main/java/com/wornux/config/TutorAiProperties.java
+++ src/main/java/com/wornux/config/TutorAiProperties.java
@@ -12,11 +12,43 @@ public class TutorAiProperties {
     private String routingModel;
     private ToolObservability toolObservability = new ToolObservability();
 
+    public String getRoutingModel() {
+        return routingModel;
+    }
+
+    public void setRoutingModel(String routingModel) {
+        this.routingModel = routingModel;
+    }
+
+    public ToolObservability getToolObservability() {
+        return toolObservability;
+    }
+
+    public void setToolObservability(ToolObservability toolObservability) {
+        this.toolObservability = toolObservability;
+    }
+
     @Getter
     @Setter
     public static class ToolObservability {
 
         private boolean capturePayloads;
         private int maxPayloadChars = 4000;
+
+        public boolean isCapturePayloads() {
+            return capturePayloads;
+        }
+
+        public void setCapturePayloads(boolean capturePayloads) {
+            this.capturePayloads = capturePayloads;
+        }
+
+        public int getMaxPayloadChars() {
+            return maxPayloadChars;
+        }
+
+        public void setMaxPayloadChars(int maxPayloadChars) {
+            this.maxPayloadChars = maxPayloadChars;
+        }
     }
 }
